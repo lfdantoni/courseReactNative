357835fbc0d551286feb7a0fbb185b96980b9607
lfdantoni <lfdantoni@gmail.com>
2017-08-13 18:05:48 -0300

    init

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+    const { textStyles } = styles;
+    return <Text style={textStyles}> Albums </Text>;
+};
+
+const styles = {
+    textStyles: {
+        fontSize: 20
+    }
+};
+
+export default Header;
